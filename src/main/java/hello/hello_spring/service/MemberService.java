5906dae2356c3d1a67bef426768240f1a8048b46
Sarang <hangpfm0518@ewhain.net>
2025-07-31 19:03:52 +0900

    섹션7 26강 스프링 통합 테스트

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -3,7 +3,6 @@ package hello.hello_spring.service;
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.repository.MemberRepository;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,7 +25,7 @@ public class MemberService {
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
-                    throw new IllegalStateException("이미 존재하는 회원");
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
     }
 
